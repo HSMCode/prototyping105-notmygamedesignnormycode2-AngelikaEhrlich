3ab28d591134fd256485e3da2c5f885b961c90b5
Angelika Ehrlich <angelikaehrlich@gmx.net>
2020-12-15 13:05:49 +0100

    make light turn on or off automatically
    make light turn on or off automatically every three seconds

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -6,23 +6,40 @@ public class Switch : MonoBehaviour
 {
     public GameObject dayTime, nightTime; //references to the day and nighttime gameobjects which i had to create and put the enemys as children
 
+    public int IsRunning = 1;       //while isRunning is "1", run script; while it is set to "0", do not do it
+    public int NumberofSeconds;     //how long it takes until the script switches
+
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-    {
-            LightswitchOnOff(); //space to switch
-            SoundScript.PlaySound(); // call the SoundScript to play the sound
+        if (IsRunning == 1)
+        {
+            StartCoroutine(Wait());
         }
     }
 
-    public void LightswitchOnOff()
+    public IEnumerator Wait()
     {
-        if (dayTime.activeInHierarchy) //if daytime is active and we press, disable daytime and enable nighttime
+        //wait number of seconds
+        IsRunning = 0;
+        yield return new WaitForSeconds(NumberofSeconds);  
+
+        //switch light
+        LightswitchOnOff();
+        SoundScript.PlaySound();
+
+        //reset
+        IsRunning = 1;
+    }
+   
+    public void LightswitchOnOff()
+   {
+        if (dayTime.activeInHierarchy) //if daytime is active and three seconds pass, disable daytime and enable nighttime
         {
             dayTime.SetActive(false);
             nightTime.SetActive(true);
         }
-        else //if daytime is inactive and we press, enable daytime and disable nighttime
+        else //if daytime is inactive and three seconds pass, enable daytime and disable nighttime
         {
             dayTime.SetActive(true);
             nightTime.SetActive(false);
